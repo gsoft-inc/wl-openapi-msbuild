09a2b3ea9a60fbca9de44b152a6fe00231d3d12e
Rushikesh Vyas <rush.vyas@gsoft.com>
2023-11-16 12:08:29 -0500

    Renamedfolder  system-tests to tests

--- Run-SystemTest.ps1
+++ Run-SystemTest.ps1
@@ -14,7 +14,7 @@ Process {
     
     $workingDir = Join-Path $PSScriptRoot "src"
     $outputDir = Join-Path $PSScriptRoot ".output"
-    $sysTestDir = Join-Path $PSScriptRoot "src/system-test/WebApi.MsBuild.SystemTest"
+    $sysTestDir = Join-Path $PSScriptRoot "src/tests/WebApi.MsBuild.SystemTest"
 
     try {
         Push-Location $workingDir
@@ -24,7 +24,6 @@ Process {
 
         Push-Location $sysTestDir
 
-        Exec { & dotnet nuget locals all --clear }
         Exec { & echo "<?xml version=`"1.0`" encoding=`"utf-8`"?><configuration><packageSources><add key=`"local-packages`" value=`"$outputDir`" /></packageSources></configuration>" > NuGet.Config }
         Exec { & dotnet add package Workleap.OpenApi.MSBuild --prerelease }
         Exec { & dotnet build -c Release }
