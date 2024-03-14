329c93b2a2d0a55bb657ba5b0a198bbff95d2fee
Mathieu Gamache <mathieu.gamache@workleap.com>
2024-03-14 13:01:59 -0400

    Rework system test

--- Run-SystemTest.ps1
+++ Run-SystemTest.ps1
@@ -5,6 +5,26 @@ Begin {
 }
 
 Process {
+    function BuildProject {
+        param (
+            [string]$openApiMsBuildSource,
+            [string]$projectPath,
+            [bool]$isFailureExpected,
+            [string]$extraArgs
+        )
+        Push-Location $projectPath
+        
+        Exec { & dotnet add package Workleap.OpenApi.MSBuild --prerelease --source $openApiMsBuildSource }
+
+        $buildProcess = Start-Process -FilePath "dotnet" -ArgumentList "build -c Release $extraArgs" -NoNewWindow -PassThru -Wait
+
+        if ($isFailureExpected -and $buildProcess.ExitCode -eq 0 ) {
+            Write-Error "The build did not fail as expected for project $projectPath."
+        } elseif (!$isFailureExpected -and $buildProcess.ExitCode -ne 0) {
+            Write-Error "The build unexpectedly failed for project $projectPath."
+        }
+    }
+
     function Exec([scriptblock]$Command) {
         & $Command
         if ($LASTEXITCODE -ne 0) {
@@ -14,6 +34,7 @@ Process {
     
     $workingDir = Join-Path $PSScriptRoot "src"
     $outputDir = Join-Path $PSScriptRoot ".output"
+    
     $contractFirstSysTestDir = Join-Path $PSScriptRoot "src/tests/WebApi.MsBuild.SystemTest.ContractFirst"
     $codeFirstSysTestDir = Join-Path $PSScriptRoot "src/tests/WebApi.MsBuild.SystemTest.CodeFirst"
     $oasDiffErrorSysTestDir = Join-Path $PSScriptRoot "src/tests/WebApi.MsBuild.SystemTest.OasDiffError"
@@ -22,28 +43,15 @@ Process {
     try {
         Push-Location $workingDir
 
+        # Build the OpenApi.MSBuild package to be used in the system tests
         Exec { & dotnet pack -c Release -o "$outputDir" }
 
-        Push-Location $contractFirstSysTestDir
-        
-        Exec { & dotnet add package Workleap.OpenApi.MSBuild --prerelease --source $outputDir }
-        Exec { & dotnet build -c Release }
-        
-        Push-Location $codeFirstSysTestDir
-        
-        Exec { & dotnet add package Workleap.OpenApi.MSBuild --prerelease --source $outputDir }
-        Exec { & dotnet build -c Release }
-
-        Push-Location $oasDiffErrorSysTestDir
-        
-        Exec { & dotnet add package Workleap.OpenApi.MSBuild --prerelease --source $outputDir }
-        Exec { & dotnet build -c Release }
-        
-        Push-Location $spectralErrorSysTestDir
-        
-        Exec { & dotnet add package Workleap.OpenApi.MSBuild --prerelease --source $outputDir }
-        Exec { & dotnet build -c Release }
-
+        BuildProject -openApiMsBuildSource $outputDir -projectPath $contractFirstSysTestDir -isFailureExpected $false
+        BuildProject -openApiMsBuildSource $outputDir -projectPath $codeFirstSysTestDir -isFailureExpected $false
+        BuildProject -openApiMsBuildSource $outputDir -projectPath $oasDiffErrorSysTestDir -isFailureExpected $true
+        BuildProject -openApiMsBuildSource $outputDir -projectPath $spectralErrorSysTestDir -isFailureExpected $true
+        BuildProject -openApiMsBuildSource $outputDir -projectPath $oasDiffErrorSysTestDir -isFailureExpected $false -extraArgs "/p:OpenApiIgnoreErrors=true"
+        BuildProject -openApiMsBuildSource $outputDir -projectPath $spectralErrorSysTestDir -isFailureExpected $false -extraArgs "/p:OpenApiIgnoreErrors=true"
     }
     finally {
         Pop-Location
