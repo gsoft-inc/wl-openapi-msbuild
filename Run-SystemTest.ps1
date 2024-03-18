7d5a6d2d6a4f546c2444d773fa0b0eeeaf6053a7
Mathieu Gamache <math.gamache@gmail.com>
2024-03-18 14:25:09 -0400

    Feature/idp 1028 treat warning as error flag (#40)
    * Add option to Treat Warning As Error (Breaking Changes)
    * Add option to opt out
    
    ---------
    
    Co-authored-by: Mathieu Gamache <mathieu.gamache@workleap.com>
    Co-authored-by: Anthony Simmon <anthony.simmon@gsoft.com>
    Co-authored-by: heqianwang <158102624+heqianwang@users.noreply.github.com>

--- Run-SystemTest.ps1
+++ Run-SystemTest.ps1
@@ -5,6 +5,26 @@ Begin {
 }
 
 Process {
+    function BuildProject {
+        param (
+            [string]$openApiMsBuildSource,
+            [string]$projectPath,
+            [bool]$isFailureExpected,
+            [string]$extraArgs
+        )
+        Push-Location $projectPath
+        
+        Exec { & dotnet add package Workleap.OpenApi.MSBuild --prerelease --source $openApiMsBuildSource }
+
+        $buildProcess = Start-Process -FilePath "dotnet" -ArgumentList "build -c Release $extraArgs" -NoNewWindow -PassThru -Wait
+
+        if ($isFailureExpected -and $buildProcess.ExitCode -eq 0 ) {
+            Write-Error "The build for project $projectPath was expected to fail, but it succeeded."
+        } elseif (!$isFailureExpected -and $buildProcess.ExitCode -ne 0) {
+            Write-Error "The build for project $projectPath was expected to succeed, but it failed."
+        }
+    }
+
     function Exec([scriptblock]$Command) {
         & $Command
         if ($LASTEXITCODE -ne 0) {
@@ -14,24 +34,24 @@ Process {
     
     $workingDir = Join-Path $PSScriptRoot "src"
     $outputDir = Join-Path $PSScriptRoot ".output"
+    
     $contractFirstSysTestDir = Join-Path $PSScriptRoot "src/tests/WebApi.MsBuild.SystemTest.ContractFirst"
     $codeFirstSysTestDir = Join-Path $PSScriptRoot "src/tests/WebApi.MsBuild.SystemTest.CodeFirst"
+    $oasDiffErrorSysTestDir = Join-Path $PSScriptRoot "src/tests/WebApi.MsBuild.SystemTest.OasDiffError"
+    $spectralErrorSysTestDir = Join-Path $PSScriptRoot "src/tests/WebApi.MsBuild.SystemTest.SpectralError"
 
     try {
         Push-Location $workingDir
 
+        # Build the OpenApi.MSBuild package to be used in the system tests
         Exec { & dotnet pack -c Release -o "$outputDir" }
 
-        Push-Location $contractFirstSysTestDir
-        
-        Exec { & dotnet add package Workleap.OpenApi.MSBuild --prerelease --source $outputDir }
-        Exec { & dotnet build -c Release -warnaserror }
-        
-        Push-Location $codeFirstSysTestDir
-        
-        Exec { & dotnet add package Workleap.OpenApi.MSBuild --prerelease --source $outputDir }
-        Exec { & dotnet build -c Release -warnaserror }
-
+        BuildProject -openApiMsBuildSource $outputDir -projectPath $contractFirstSysTestDir -isFailureExpected $false
+        BuildProject -openApiMsBuildSource $outputDir -projectPath $codeFirstSysTestDir -isFailureExpected $false
+        BuildProject -openApiMsBuildSource $outputDir -projectPath $oasDiffErrorSysTestDir -isFailureExpected $true
+        BuildProject -openApiMsBuildSource $outputDir -projectPath $spectralErrorSysTestDir -isFailureExpected $true
+        BuildProject -openApiMsBuildSource $outputDir -projectPath $oasDiffErrorSysTestDir -isFailureExpected $false -extraArgs "/p:OpenApiTreatWarningsAsErrors=false"
+        BuildProject -openApiMsBuildSource $outputDir -projectPath $spectralErrorSysTestDir -isFailureExpected $false -extraArgs "/p:OpenApiTreatWarningsAsErrors=false"
     }
     finally {
         Pop-Location
