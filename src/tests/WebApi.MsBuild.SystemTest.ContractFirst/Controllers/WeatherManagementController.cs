b814c434c57eb9738a0c7eed5734099c95180f04
Mathieu Gamache <math.gamache@gmail.com>
2024-03-20 11:05:39 -0400

    [IDP-1044] Update ruleset (#43)
    * Update ruleset (Breaking Changes)
    
    ---------
    
    Co-authored-by: Mathieu Gamache <mathieu.gamache@workleap.com>

--- src/tests/WebApi.MsBuild.SystemTest.ContractFirst/Controllers/WeatherManagementController.cs
+++ src/tests/WebApi.MsBuild.SystemTest.ContractFirst/Controllers/WeatherManagementController.cs
@@ -12,19 +12,19 @@ public class WeatherManagementController : ControllerBase
     {
         "Accuweather", "AerisWeather", "Foreca", "Open Weathermap", "National Oceanic and Atmospheric Administration",
     };
-    
+
     private readonly ILogger<WeatherManagementController> _logger;
-    
+
     public WeatherManagementController(ILogger<WeatherManagementController> logger)
     {
         this._logger = logger;
     }
-    
+
     [HttpGet(Name = "GetWeatherSources")]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status403Forbidden)]
-    public IEnumerable<string> Get()
+    public IEnumerable<WeatherSource> Get()
     {
-        return Sources;
+        return Sources.Select(x => new WeatherSource { Source = x });
     }
 }
