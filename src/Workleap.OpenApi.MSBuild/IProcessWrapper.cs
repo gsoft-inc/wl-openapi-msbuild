b714df9de9d9ec94efde6b1c4a5adb6d64210f11
He Qian Wang <heqian.wang@gsoft.com>
2025-02-27 10:06:18 -0500

    Upgrade wl-api-guidelines after workleap rename

--- src/Workleap.OpenApi.MSBuild/IProcessWrapper.cs
+++ src/Workleap.OpenApi.MSBuild/IProcessWrapper.cs
@@ -4,5 +4,5 @@ namespace Workleap.OpenApi.MSBuild;
 
 internal interface IProcessWrapper
 {
-    public Task<BufferedCommandResult> RunProcessAsync(string filename, string[] arguments, CancellationToken cancellationToken, Dictionary<string, string?>? envVars = null);
+    Task<BufferedCommandResult> RunProcessAsync(string filename, string[] arguments, CancellationToken cancellationToken, Dictionary<string, string?>? envVars = null);
 }
--- src/Workleap.OpenApi.MSBuild/Spectral/SpectralRulesetManager.cs
+++ src/Workleap.OpenApi.MSBuild/Spectral/SpectralRulesetManager.cs
@@ -5,7 +5,7 @@ namespace Workleap.OpenApi.MSBuild.Spectral;
 
 internal sealed class SpectralRulesetManager
 {
-    private const string WorkleapRulesetVersion = "0.9.0";
+    private const string WorkleapRulesetVersion = "0.10.1";
     private const string WorkleapRulesetDownloadUrlFormat = "https://raw.githubusercontent.com/workleap/wl-api-guidelines/{0}/.spectral.{1}.yaml";
 
     private readonly ILoggerWrapper _loggerWrapper;
